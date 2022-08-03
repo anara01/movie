1505e8106e6496d3c011a30f6ab33d5cf6ff7377
anara01 <anaraerkebaeva2@gmail.com>
2022-08-03 16:59:19 +0300

    отзывы и кадры из фильма

--- movies/urls.py
+++ movies/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', MoviesView.as_view()),
     path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
+    path("review/<int:pk>/", AddReview.as_view(), name='add_review'),
 ]
