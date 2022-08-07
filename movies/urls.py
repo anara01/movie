f08227cac2af215aa5404357add3c7114e808adf
anara01 <anaraerkebaeva2@gmail.com>
2022-08-07 20:01:30 +0300

    ajax filter + hogan.js

--- movies/urls.py
+++ movies/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', MoviesView.as_view()),
     path("filter/", FilterMovieView.as_view(), name='filter'),
+    path("json-filter/", JsonFilterMoviesView.as_view(), name='json_filter'),
     path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>/", AddReview.as_view(), name='add_review'),
     path("actor/<str:slug>/", ActorView.as_view(), name='actor_detail'),
