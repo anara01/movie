4a5910363a2b0efdaf2c1819089bed24c5c5711a
anara01 <anaraerkebaeva2@gmail.com>
2022-08-07 19:03:23 +0300

    filter movies Q

--- movies/urls.py
+++ movies/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', MoviesView.as_view()),
+    path("filter/", FilterMovieView.as_view(), name='filter'),
     path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>/", AddReview.as_view(), name='add_review'),
-    path("actor/<str:slug>/", ActorView.as_view(), name='actor_detail')
+    path("actor/<str:slug>/", ActorView.as_view(), name='actor_detail'),
 ]
