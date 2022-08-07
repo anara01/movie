28c5380a0c37efbcf91ef672870e19efd43617f3
anara01 <anaraerkebaeva2@gmail.com>
2022-08-07 18:25:47 +0300

    page actor & related_name

--- movies/urls.py
+++ movies/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', MoviesView.as_view()),
     path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>/", AddReview.as_view(), name='add_review'),
+    path("actor/<str:slug>/", ActorView.as_view(), name='actor_detail')
 ]
